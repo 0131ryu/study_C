2a5aea6eb9a812195226f09b2d9f0600a8ebcf04
0131ryu <131ryuji@gmail.com>
2023-08-07 21:34:19 +0900

    배열 관련 연습문제 복습

---
+++ chapter08/example/273.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int aList[5] = {30, 40, 10, 50, 20};
+    int i = 0, nTmp = 0;
+
+    // 코드
+    for (i = 0; i < 5; ++i)
+        if (aList[i] < aList[0])
+        {
+            aList[0] = nTmp;
+            nTmp = aList[i];
+            aList[0] = aList[i];
+        }
+
+    for (i = 0; i < 5; ++i)
+        printf("%d\t", aList[i]);
+    putchar('\n');
+
+    printf("MIN : %d\n", aList[0]);
+    return 0;
+}
